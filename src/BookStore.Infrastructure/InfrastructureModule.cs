43606bce8dd3788c1a15ebacc9c0458ea8859703
M-Alzanati <mohamed.h.alzanaty@gmail.com>
2021-04-12 11:30:42 +0200

    add infrastructure module

---
+++ src/BookStore.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Reflection;
+using Autofac;
+using BookStore.Core;
+using BookStore.Infrastructure.Data;
+using BookStore.SharedKernel.Interfaces;
+
+namespace BookStore.Infrastructure
+{
+    public class InfrastructureModule : Autofac.Module
+    {
+        private List<Assembly> _assemblies = new List<Assembly>();
+
+        public InfrastructureModule(Assembly callingAssembly = null)
+        {
+            var coreAssembly = Assembly.GetAssembly(typeof(DatabasePopulator));
+            var infrastructureAssembly = Assembly.GetAssembly(typeof(EfRepository));
+
+            _assemblies.Add(coreAssembly);
+            _assemblies.Add(infrastructureAssembly);
+            if (callingAssembly != null)
+            {
+                _assemblies.Add(callingAssembly);
+            }
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfRepository>().As<IRepository>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
