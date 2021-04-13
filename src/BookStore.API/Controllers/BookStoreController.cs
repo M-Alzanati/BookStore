0486cc4e85257317c54fad07a92a6ba3cf1ac723
M-Alzanati <mohamed.h.alzanaty@gmail.com>
2021-04-13 03:49:17 +0200

    add basic implementation book store controller

--- src/BookStore.API/Controllers/BookStoreController.cs
+++ src/BookStore.API/Controllers/BookStoreController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace BookStore.API.Controllers
 {
+    [Authorize]
     public class BookStoreController : BaseApiController
     {
         private readonly ILogger<BookStoreController> _logger;
@@ -14,6 +16,34 @@ namespace BookStore.API.Controllers
         public BookStoreController(ILogger<BookStoreController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("books/add")]
+        public async Task<IActionResult> AddBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("books")]
+        public async Task<IActionResult> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("books/{name}")]
+        public async Task<IActionResult> GetBooks([FromRoute] string bookName)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("reviews/add")]
+        public async Task<IActionResult> AddReview()
+        {
+            throw new NotImplementedException();
         }
     }
 }
