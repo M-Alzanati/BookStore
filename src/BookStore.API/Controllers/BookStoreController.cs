54c324b13f49ca87702d967f9ed9d0addd4bc47d
M-Alzanati <mohamed.h.alzanaty@gmail.com>
2021-04-14 04:29:23 +0200

    implement action methods in bookstore controller

--- src/BookStore.API/Controllers/BookStoreController.cs
+++ src/BookStore.API/Controllers/BookStoreController.cs
@@ -5,45 +5,76 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using BookStore.API.ApiModels;
+using BookStore.SharedKernel.Interfaces;
+using BookStore.Core.Interfaces;
+using BookStore.Core.Entities;
 
 namespace BookStore.API.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class BookStoreController : BaseApiController
     {
         private readonly ILogger<BookStoreController> _logger;
 
-        public BookStoreController(ILogger<BookStoreController> logger)
+        private readonly IRepository _repository;
+
+        private readonly ITenantService _tenantService;
+
+        public BookStoreController(ILogger<BookStoreController> logger, IRepository repository, ITenantService tenantService)
         {
             _logger = logger;
+            _repository = repository;
+            _tenantService = tenantService;
         }
 
         [HttpPost]
         [Route("books/add")]
-        public async Task<IActionResult> AddBook()
+        public async Task<IActionResult> AddBook([FromBody] BookModelDTO model)
         {
-            throw new NotImplementedException();
+            if (ModelState.IsValid)
+            {
+                var tenantId = _tenantService.GetTenantId();
+                var book = BookModelDTO.FromBookDTO(model);
+                var addedBook = await _repository.AddAsync<Book>(book);
+                return (Ok(addedBook));
+            }
+
+            return BadRequest(ModelState);
         }
 
         [HttpGet]
-        [Route("books")]
+        [Route("books/all")]
         public async Task<IActionResult> GetBooks()
         {
-            throw new NotImplementedException();
+            var tenantId = _tenantService.GetTenantId();
+            var allBooks = await _repository.ListAsync<Book>();
+            return Ok(BookModelDTO.FromBook(allBooks));
         }
 
         [HttpGet]
         [Route("books/{name}")]
-        public async Task<IActionResult> GetBooks([FromRoute] string bookName)
+        public async Task<IActionResult> GetBook([FromRoute] string name)
         {
-            throw new NotImplementedException();
+            var tenantId = _tenantService.GetTenantId();
+            var myBook = await _repository.GetByIdAsync<Book>(book => book.Name == name);
+            return Ok(BookModelDTO.FromBook(myBook));
         }
 
         [HttpPost]
         [Route("reviews/add")]
-        public async Task<IActionResult> AddReview()
+        public async Task<IActionResult> AddReview([FromBody] ReviewModelDTO review)
         {
-            throw new NotImplementedException();
+            if (ModelState.IsValid)
+            {
+                var tenantId = _tenantService.GetTenantId();
+                var myReview = ReviewModelDTO.FromReviewDTO(review);
+                var addedReview = await _repository.AddAsync<Review>(myReview);
+                return Ok(addedReview);
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
