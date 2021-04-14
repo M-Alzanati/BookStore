0604019b39b02acc26e40bc1f61029b18ad1c330
M-Alzanati <mohamed.h.alzanaty@gmail.com>
2021-04-14 04:28:55 +0200

    validate model in auth controller

--- src/BookStore.API/Controllers/AuthenticateController.cs
+++ src/BookStore.API/Controllers/AuthenticateController.cs
@@ -31,54 +31,64 @@ namespace BookStore.API.Controllers
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginModelDTO model)
         {
-            var user = await _userManager.FindByNameAsync(model.Email);
-            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            if (ModelState.IsValid)
             {
-                var authClaims = new List<Claim>
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var authClaims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, user.UserName),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 };
 
-                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
 
-                var token = new JwtSecurityToken
-                (
-                    issuer: _configuration["Jwt:Issuer"],
-                    audience: _configuration["Jwt:Issuer"],
-                    expires: DateTime.Now.AddHours(3),
-                    claims: authClaims,
-                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-                );
+                    var token = new JwtSecurityToken
+                    (
+                        issuer: _configuration["Jwt:Issuer"],
+                        audience: _configuration["Jwt:Issuer"],
+                        expires: DateTime.Now.AddHours(3),
+                        claims: authClaims,
+                        signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
 
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token),
-                    expiration = token.ValidTo
-                });
+                    return Ok(new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    });
+                }
+                return Unauthorized();
             }
-            return Unauthorized();
+
+            return BadRequest(ModelState);
         }
 
         [HttpPost]
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModelDTO model)
         {
-            var userExists = await _userManager.FindByNameAsync(model.Email);
-            if (userExists != null)
-                return BadRequest("User already exists!");
-
-            ApplicationUser user = new ApplicationUser()
+            if (ModelState.IsValid)
             {
-                Email = model.Email,
-                SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = model.Email,
-            };
-            var result = await _userManager.CreateAsync(user, model.Password);
-            if (!result.Succeeded)
-                return BadRequest("User creation failed! Please check user details and try again.");
+                var userExists = await _userManager.FindByNameAsync(model.Email);
+                if (userExists != null)
+                    return BadRequest("User already exists!");
 
-            return Ok("User created successfully!");
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = model.Email,
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (!result.Succeeded)
+                    return BadRequest("User creation failed! Please check user details and try again.");
+
+                return Ok("User created successfully!");
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
